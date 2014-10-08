61d744ebfc5df15c4792236bdc6ad4d44fb3b679
Ann Gunter <amichele505@gmail.com>
2014-10-08 00:11:06 -0600

    Improvements to animation
    Streamlined and commented code, some updates still needed.

--- js/ann.js
+++ js/ann.js
@@ -1,14 +1,5 @@
 (function() {
 
-/*  $('.ann-image').on({
-  	mouseenter: function() {
-  		$(this).transition( {scale: '1.2' } );
-  	}, 
-    mouseleave: function() {
-  		$(this).removeAttr('style');
-  	}
-  } );*/
-
   /*front page doggy animation*/
   function doggy( picID, picA, picB, period) {
     var picElem = document.getElementById( picID ), state = false;
@@ -25,142 +16,100 @@
 } ) ();
 
 
-/*for animated squares on projects page*/
+/*
+Functions for animated squares on projects page
+*/
+
 (function() {
 
   /*sets size of grid on click of set button*/
-  $("#setSquares").click( function(event) {
+  $("#setSize").on('change', function(event) {
 
     /*declare variables and set counter*/
     var annSquare = $(".annSquare");
       firstSquare = $(".firstSquare");
-      message = $("#message");
       setSize = $("#setSize");
       parentDiv = $('#parentDiv');
       counter = setSize.val();
 
-    /*reset the area and message by removing items*/
+    /*reset the area by removing divs*/
     annSquare.remove();
     firstSquare.remove();
-    message.remove();
 
-    /*prevents button click from reloading page*/
-    event.preventDefault();
-
-    /*validate entry to be between one and ten*/
-    if ( counter <= 10 && counter >= 1) {
-
-      /*displays the grid size*/
-      $("#displayEntry").append("<p id='message'>");
-      $("#message").append("You entered " + counter);
-
-      /*adds a row and column for each iteration*/
-      for (i = 0; i < counter; i++) {
-        for (j = 0; j < counter; j++) {
-          if (j === 0) {
-          parentDiv.append("<div class='firstSquare anisquare'>");
-          } else {
-          parentDiv.append("<div class='annSquare anisquare'>");
-          };
+    /*i sets columns and j sets rows*/
+    for (i = 0; i < counter; i++) {
+      for (j = 0; j < counter; j++) {
+        if (j === 0) {
+        parentDiv.append("<div class='firstSquare anisquare'>");
+        } else {
+        parentDiv.append("<div class='annSquare anisquare'>");
         };
       };
-    } else {
-      /*must enter a valid number*/
-      alert("Please enter a number between one and ten.");
     };
-
-    /*after running, removes value from text box*/
-    setSize.val("");
-
   });
-
-  /*resets squares on click of reset button*/
-  $("#resetSquares").click( function() {
-
-    /*set variables*/
-    var annSquare = $(".annSquare");
-      firstSquare = $(".firstSquare");
-      message = $("#message");
-      setSize = $("#setSize");
-
-    /*prevents button click from reloading page*/
-    event.preventDefault();
-    /*removed all divs added by the button and removes the value from the field*/
-    annSquare.remove();
-    firstSquare.remove();
-    message.remove();
-    setSize.val("");
-  });
-
 }) ();
 
+
+/*all functions related to animation of squares on project page*/
 (function() {
 
-  function clearout() {
-    console.log("clearout called");
-    $("#parentDiv").off('mouseenter');
-    $(".anisquare")
-      .transition( {rotate: '0deg', perspective: '0px', rotateX:'0deg', rotateY:'0deg'} );
-  }
+  /*on click of anibutton runs getAnimation function, which sets the animation of the squares based on the selected radio button*/
+  $(".anibutton").on('click', getAnimation);
+
+  function getAnimation() {
+    /*gets the selected radio button*/
+    var selected = $("input[type='radio'][name='anitype']:checked");
+
+    /* switch statement controlled by the value of the selected radio button*/
+    switch (selected.val()) {
+      case "spin":
+        /*calls spin function*/
+        spin();
+        break;
+      case "hFlip":
+        /*calls hflip function*/
+        hFlip();
+        break;
+      case "vFlip":
+        /*calls vflip function*/
+        vFlip();
+        break;
+      default:
+        break;
+    };
+  };
 
   function spin() {
+    /*removes mouseenter bind*/
     $("#parentDiv").off('mouseenter');
+    /*spins squares on mouseenter and resets other transitions*/
     $("#parentDiv").on('mouseenter', '.anisquare', function() {
       $(this)
         .removeAttr('style')
         .transition( {rotate: '360deg', perspective: '0px', rotateX:'0deg', rotateY:'0deg'} )
-        console.log("spin running");  /*test script*/
     } );
-  }
+  };
 
   function hFlip() {
+    /*removes mouseenter bind*/
     $("#parentDiv").off('mouseenter');
+    /*flips squares on the x axis on mouseenter and resets other transitions*/
     $("#parentDiv").on('mouseenter', '.anisquare', function() {
       $(this)
         .removeAttr('style')
         .transition( {perspective: '100px', rotateX:'180deg', rotateY:'0deg', rotate:'0deg' } )
-        console.log("hFlip running");  /*test script*/;
     } );
-  }
+  };
 
   function vFlip() {
+    /*removes mouseenter bind*/
     $("#parentDiv").off('mouseenter');
+    /*flips squares on the y axis on mouseenter and resets other transitions*/
     $("#parentDiv").on('mouseenter', '.anisquare', function() {
       $(this)
         .removeAttr('style')
         .transition( {perspective: '100px', rotateY:'180deg', rotateX:'0deg', rotate:'0deg' } )
-      console.log("vFlip running");  /*test script*/
     } );
-  }
-
-  function getAnimation() {
-
-    /*$( "#hidediv" ).hide();*/
-    /*$("#resetAnimation").show();
-    console.log("hiding");*/
-    /*console.log("the value is " + $(".anibutton").attr('id'));*/
-
-    var selected = $("input[type='radio'][name='anitype']:checked");
-    console.log("selected value is " + selected.val());
-
-    switch (selected.val()) {
-      case "spin":
-        console.log("spinning");
-        spin();
-        break;
-      case "hFlip":
-        spinbool = false;
-        console.log("hflipping");
-        hFlip();
-        break;
-      case "vFlip":
-        console.log("vflipping");
-        vFlip();
-        break;
-    };
-  }
-
-  $("#resetAnimation").hide();
-  $(".anibutton").on('click', getAnimation);
-
+  };
 }) ();
+
